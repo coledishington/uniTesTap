65a1e2389545245c587d82f33abd48d56c4d1235
Cole Dishington <coledishington@gmail.com>
2023-12-04 13:55:43 +1300

    Run tap tests using automake tap driver
    Run tap tests through the automake distributed tap driver. This will
    verify the output of the tap producer library to simplify verification,
    however, as some of the tap library tests are meant to fail the reported
    stats are not correct.

--- autogen.sh
+++ autogen.sh
@@ -12,6 +12,14 @@ set -xe
 
     mkdir -p "$AUX_DIR" "$M4_DIR"
 
+    # Go looking for the tap-driver if it is missing
+    if ! [ -e "$AUX_DIR/tap-driver.sh" ]; then
+        if ! AUTOMAKE_LIBDIR=$(automake --print-libdir); then
+            printf 'automake error: cannot find tap-driver.sh' >&2
+        fi
+        cp "$AUTOMAKE_LIBDIR/tap-driver.sh" "$AUX_DIR"
+    fi
+
     autoreconf -ivf
 
     cd build
