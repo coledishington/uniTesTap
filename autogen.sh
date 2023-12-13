fbb9cc325969690c54b97a8ad5574d7d384cf156
Cole Dishington <coledishington@gmail.com>
2023-12-13 14:58:16 +1300

    Stop changing the working directory of the caller of autogen.sh
    Wrap the remainder of the script in a sub-shell to avoid changing the
    working directory of the caller of autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+BUILD_DIR=build
+AUX_DIR="$BUILD_DIR/autotools/aux"
+M4_DIR="$BUILD_DIR/autotools/m4"
+
 set -xe
 
-# Must be run from source directory
-cd "${0%autogen.sh}"
-
-mkdir -p build/autotools/aux build/autotools/m4
-
-autoreconf -ivf
-
 (
+    # Must be run from source directory
+    cd "${0%autogen.sh}"
+
+    mkdir -p "$AUX_DIR" "$M4_DIR"
+
+    autoreconf -ivf
+
     cd build
     ../configure
     make
