58c143c7e642e0cd9889b4253571c8045d040612
Cole Dishington <coledishington@gmail.com>
2023-12-13 15:14:15 +1300

    Support builds in source tree
    Support building the project in the source tree. Continue to default to
    building in a ./build directory.

--- autogen.sh
+++ autogen.sh
@@ -1,14 +1,59 @@
 #!/bin/sh
 
+# Autoconf aux and m4 will always go in <project-root>/build
+AUX_DIR="build/autotools/aux"
+M4_DIR="build/autotools/m4"
+PROJECT_ROOT=${0%autogen.sh}
 BUILD_DIR=build
-AUX_DIR="$BUILD_DIR/autotools/aux"
-M4_DIR="$BUILD_DIR/autotools/m4"
 
-set -xe
+help() {
+    cat <<eof
+usage: autogen.sh [-h|OPTION]... [builddir]
+Build uniTesTap
 
-(
+positional arguments:
+  builddir                   The root of the build tree. (default: <project-root>/build)
+
+options:
+  -h, --help                 show this help message and exit
+eof
+}
+
+argparse() {
+    N_POSITIONAL_ARGS=0
+
+    while [ $# -gt 0 ]; do
+        case "$1" in
+            -h | --help)
+                HELP='yes'
+                shift
+                ;;
+            -*)
+                printf 'Unknown option %s\n' "$1" >&2
+                return 1
+                ;;
+            *)
+                if [ $N_POSITIONAL_ARGS -eq 0 ]; then
+                    BUILD_DIR="$1"
+                else
+                    printf 'Unknown positional argument: %s' "$1" >&2
+                fi
+                N_POSITIONAL_ARGS=$((N_POSITIONAL_ARGS + 1))
+                shift
+                ;;
+        esac
+    done
+
+    [ $N_POSITIONAL_ARGS -le 1 ]
+}
+
+build() (
+    # All build steps must be successful
+    set -xe
+
     # Must be run from source directory
-    cd "${0%autogen.sh}"
+    cd "$PROJECT_ROOT"
+    ABS_PROJECT_ROOT=$(pwd)
 
     mkdir -p "$AUX_DIR" "$M4_DIR"
 
@@ -21,8 +66,20 @@ set -xe
     fi
 
     autoreconf -ivf
-
-    cd build
-    ../configure
+    cd "$BUILD_DIR"
+    "$ABS_PROJECT_ROOT"/configure
     make
 )
+
+if ! argparse "$@"; then
+    exit 1
+fi
+
+mkdir -p "$BUILD_DIR"
+
+if [ "$HELP" = 'yes' ]; then
+    help
+    exit 1
+fi
+
+build
