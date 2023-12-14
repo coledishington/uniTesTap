098684c8321e3c9bfe9dca0d6ca3ebc2098f3972
Cole Dishington <coledishington@gmail.com>
2023-12-14 18:34:27 +1300

    Support processing test output into TAP comments
    Capture test output stdout/stderr and print as TAP comments

--- src/tap.c
+++ src/tap.c
@@ -54,19 +54,51 @@ static void tap_report_test(size_t test_id, struct test_cfg *test_cfg,
     }
 }
 
+static void tap_process_test_output(int test_fd) {
+    size_t line_len = 0;
+    char *line = NULL;
+    ssize_t bytes;
+    FILE *test_fp;
+
+    test_fp = fdopen(test_fd, "r");
+    for (; (bytes = getline(&line, &line_len, test_fp)) != -1;) {
+        if (bytes == 0 || *line == '\n') {
+            continue;
+        }
+        /* Print test output as TAP comment */
+        printf("# %s", line);
+    }
+    free(line);
+}
+
 static void tap_run_test_and_exit(struct test_cfg *test_cfg) {
     int res;
 
     res = test_cfg->test();
+    fflush(NULL);
     _exit(res);
 }
 
 static int tap_evaluate(size_t test_id, struct test_cfg *test_cfg) {
-    int wres;
+    int pipefd[2] = {-1, -1};
+    int res, wres;
     pid_t cpid;
 
+    res = pipe(pipefd);
+    if (res == -1) {
+        int err = errno;
+        printf("# failed to create pipe %s(%d)\n", strerror(err), err);
+        return err;
+    }
+
+    /* Flush stdout and stderr to avoid child duplicating buffered output */
+    fflush(NULL);
     cpid = fork();
     if (cpid == 0) {
+        close(pipefd[0]); /* Close read side */
+        dup2(pipefd[1], STDOUT_FILENO);
+        dup2(pipefd[1], STDERR_FILENO);
+        close(pipefd[1]);
         /* Child process will run test and exit */
         tap_run_test_and_exit(test_cfg);
         _exit(-1);
@@ -76,8 +108,14 @@ static int tap_evaluate(size_t test_id, struct test_cfg *test_cfg) {
         /* Child process spawning failed */
         printf("# internal test runner error %s(%d)\n", strerror(err), err);
         tap_print_testpoint(false, test_id, NULL);
+        close(pipefd[0]);
+        close(pipefd[1]);
         return err;
     }
+    close(pipefd[1]);
+
+    tap_process_test_output(pipefd[0]);
+    close(pipefd[0]);
 
     /* Wait for child to exit */
     cpid = waitpid(cpid, &wres, 0);
