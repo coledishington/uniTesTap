4eb4a421c95323facda8c9fb34fff177783fb17c
Cole Dishington <coledishington@gmail.com>
2023-12-21 18:33:15 +1300

    Add library state handle to support re-entrant function calls
    Add library handle to support re-entrant function calls whilst
    maintaining state between calls.

--- tapcore/tests/test_metadata.c
+++ tapcore/tests/test_metadata.c
@@ -1,9 +1,10 @@
+#include <stdlib.h>
 #include <tap.h>
 
 #include "internal.h"
 
 int main(void) {
-    tap_register(pass, "Test description");
-    tap_register(fail, "Test description");
-    tap_runall();
+    tap_register(NULL, pass, "Test description");
+    tap_register(NULL, fail, "Test description");
+    tap_runall_and_cleanup(NULL);
 }
--- tapcore/tests/test_testplan_fail.c
+++ tapcore/tests/test_testplan_fail.c
@@ -4,6 +4,6 @@
 #include "internal.h"
 
 int main(void) {
-    tap_register(fail, NULL);
-    tap_runall();
+    tap_register(NULL, fail, NULL);
+    tap_runall_and_cleanup(NULL);
 }
